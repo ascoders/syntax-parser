a8539133062b0be278b4da910a3dd8aaf1fd5116
ziyi.hzy <ziyi.hzy@alibaba-inc.com>
2018-12-24 14:33:22 +0800

    Prepare to update.

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,4 +1,4 @@
 export { chain, createParser } from './chain';
 export { many, matchTokenType, optional, plus } from './match';
 export { Scanner } from './scanner';
-export { ChainFunction, ChainNode, IParseResult } from './define';
+export * from './define';
